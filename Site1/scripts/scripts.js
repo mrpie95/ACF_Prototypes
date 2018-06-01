e24537b30202946e32fe18dae1e977bc21b0290e
mrpie95 <michael.pieniazek@gmail.com>
2018-06-01 18:00:25 +1000

    first prototype

--- Site1/scripts/scripts.js
+++ Site1/scripts/scripts.js
@@ -15,8 +15,22 @@ function myMap()
   marker.setMap(map);
 }
 
+
+function openMenu(id) {
+  for (var i = 1; i <= 6; i++) {
+    if (id == i){
+      document.getElementById("menu"+id).style.display = "block";
+      document.getElementById("menu"+id).className += " w3-animate-opacity";
+    }
+    else{
+      document.getElementById("menu"+i).style.display = "none";
+      document.getElementById("menu"+i).className += " w3-animate-opacity";
+    }
+  }
+}
+
 // Modal Image Gallery
-function onClick(element) {
+function openMessage(element) {
   document.getElementById("img01").src = element.src;
   document.getElementById("modal01").style.display = "block";
   var captionText = document.getElementById("img01");
@@ -25,14 +39,19 @@ function onClick(element) {
 
 // Change style of navbar on scroll
 window.onscroll = function() {myFunction()};
+
+
 function myFunction() {
     var navbar = document.getElementById("myNavbar");
-    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
         navbar.className = "w3-bar" + " w3-card" + " w3-animate-top" + " w3-white";
+        navbar.style.display = "block";
     } else {
         navbar.className = navbar.className.replace(" w3-card w3-animate-top w3-white", "");
+        navbar.style.display = "none";
     }
 }
+
 
 // Used to toggle the menu on small screens when clicking on the menu button
 function toggleFunction() {
@@ -43,3 +62,37 @@ function toggleFunction() {
         x.className = x.className.replace(" w3-show", "");
     }
 }
+
+jQuery(document).ready(function() {
+ 
+var offset = 250;
+ 
+var duration = 300;
+ 
+jQuery(window).scroll(function() {
+ 
+if (jQuery(this).scrollTop() > offset) {
+ 
+jQuery('.back-to-top').fadeIn(duration);
+ 
+} else {
+ 
+jQuery('.back-to-top').fadeOut(duration);
+ 
+}
+ 
+});
+ 
+ 
+ 
+jQuery('.back-to-top').click(function(event) {
+ 
+event.preventDefault();
+ 
+jQuery('html, body').animate({scrollTop: 0}, duration);
+ 
+return false;
+ 
+})
+ 
+});
